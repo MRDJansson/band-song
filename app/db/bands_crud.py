88fca4163040fc0115c9b460fc3c452aee42f256
Daniel Jansson <148873761+MRDJansson@users.noreply.github.com>
2025-03-24 17:54:13 +0200

    bands_crud alustus valmis, seuraavaksi funktiot routeriin

--- app/db/bands_crud.py
+++ app/db/bands_crud.py
@@ -15,3 +15,8 @@ def read_all_bands(name: str = ""):
 def read_band_by_id(band_id: int):
     bid = [b for b in bands if b["id"] == band_id]
     if len(bid) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"Band with the iID of: {band_id} was not found."
+        )
+    return bid[0]
+
