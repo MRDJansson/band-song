9158bd557719eb61d0ddc4425de63889c4fb68ac
Daniel Jansson <148873761+MRDJansson@users.noreply.github.com>
2025-03-24 18:04:15 +0200

    bands_crud pohja valmis, katsotaan loput ominaisuudet ja tiedot myöhemmin

--- app/db/bands_crud.py
+++ app/db/bands_crud.py
@@ -20,3 +20,17 @@ def list_band_by_id(band_id: int):
         )
     return bid[0]
 
+def save_band(band_in: BandIn):
+    new_id = len(bands)
+    band = BandOut(**band_in.model_dump(), id=new_id)
+    bands.append(band.model_dump())
+    return band
+
+def delete_band_by_id(band_id: int):
+    bid = [b for b in bands if b["id"] == band_id]
+    if len(bid) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"Band with the iID of: {band_id} was not found."
+        )
+    del bands[band_id]
+    return {"message": f"Band with the ID of {band_id} was deleted."}
--- app/routers/bands.py
+++ app/routers/bands.py
@@ -12,3 +12,6 @@ def list_all_bands(name: str = ""):
 def list_bands_by_id(band_id: int):
     return bands_crud.list_all_bands(band_id)
 
+@router.post("/", response_model=BandOut, status_code=status.HTTP_201_CREATED)
+def save_band(band_in: BandIn):
+    return bands_crud.save
