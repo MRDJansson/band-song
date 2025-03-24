af7444a23ef55e006787d2163718f82a29f8f483
Daniel Jansson <148873761+MRDJansson@users.noreply.github.com>
2025-03-24 17:52:09 +0200

    Harjoitusprojektin aloitus

---
+++ app/db/bands_crud.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException
+from ..db.models import BandIn, BandOut
+
+bands = [
+    {"id": "name": "Children of Bodom"},
+    {"id": "name": "Lorna Shore"},
+]
+
+def read_all_bands(name: str = ""):
+    if name == "":
+        return bands
+    return [b for b in bands if b["name"] == name]
+
+
+def read_band_by_id(band_id: int):
+    bid = [b for b in bands if b["id"] == band_id]
+    if len(bid) == 0:
---
+++ app/db/models.py
@@ -0,0 +1,23 @@
+from sqlmodel import SQLModel, Field
+
+class BaseSong(SQLModel):
+    song: str
+    band: str
+    
+class SongIn(BaseSong):
+    pass
+
+class SongOut(BaseSong):
+    id: int
+    
+class BandBase(SQLModel):
+    name: str
+    
+class BandIn(BandBase):
+    pass
+
+class BandOut(BandBase):
+    id: int
+
+class BandDb(BaseSong, table=True):
+    id: int = Field(default=None, primary_key=True)
